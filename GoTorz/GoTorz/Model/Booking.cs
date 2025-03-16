eb85a40a2308b054c32de47e13685ea32de745a5
Scharla <dsan62737>
2025-03-16 22:54:47 +0100

    Asynkron afsluttet

---
+++ GoTorz/GoTorz/Model/Booking.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace GoTorz.Model
+{
+    public class Booking
+    {
+        public string? BookingId { get; set; }
+        public string? PackageId { get; set; }
+        public List<Customer> Customers { get; set; }
+        public Booking(List<Customer> customers)
+        {
+            Customers = customers;
+        }
+    }
+    public class Customer
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string FirstName { get; set; } = "";  // non-nullable and initialized
+
+        [StringLength(100)]
+        public string? LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Phone]
+        public string? Phone { get; set; }
+
+        public bool? IsPaid { get; set; }
+
+        [Required]
+        public string Age { get; set; } = "";
+    }
+}
