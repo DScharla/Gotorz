db98cd2c839f4a567f5877e6117e4ff1898e0134
Scharla <dsan62737>
2025-03-17 09:50:07 +0100

    Ting er gjort

---
+++ GoTorz/GoTorz/Components/ThemeSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace GoTorz.Components
+{
+    public class ThemeSettings
+    {
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+        public bool IsDarkMode { get; set; }
+    }
+}
---
+++ GoTorz/GoTorz/Components/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace GoTorz.Components
+{
+    public class UserInfo
+    {
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public bool PrefersDarkMode { get; set; }
+    }
+}
