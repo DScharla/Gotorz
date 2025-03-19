97ccbf954c99d7a8a7250ce4fa1708f5c98b1c81
Christina Løvenholt Andersen <clan63283@edu.ucl.dk>
2025-03-19 11:39:21 +0100

    slettet irrelevante properties i Test metode

--- GoTorz/GoTorz/Services/OfferService.cs
+++ GoTorz/GoTorz/Services/OfferService.cs
@@ -67,33 +67,18 @@ namespace GoTorz.Services
                 Origin = "LHR",
                 Destination = "JFK",
                 DepartureDate = "2025-04-24",
-                DepartureTime = new TimeRange { From = "09:45", To = "17:00" },
-                ArrivalTime = new TimeRange { From = "09:45", To = "17:00" }
             }
-        },
-                    PrivateFares = new Dictionary<string, List<PrivateFare>>
-        {
-            { "QF", new List<PrivateFare> { new PrivateFare { CorporateCode = "FLX53", TrackingReference = "ABN:2345678" } } },
-            { "UA", new List<PrivateFare> { new PrivateFare { CorporateCode = "1234", TourCode = "578DFL" } } }
-        },
+        },                    
                     Passengers = new List<PassengerRequest>
         {
             new PassengerRequest
             {
-                GivenName = "Amelia",
-                FamilyName = "Earhart",
-                Type = "adult",
-                LoyaltyProgrammeAccounts = new List<LoyaltyProgrammeAccount>
-                {
-                    new LoyaltyProgrammeAccount { AccountNumber = "12901014", AirlineIataCode = "BA" }
-                }
+                Type = "adult",                
+                
             },
-            new PassengerRequest { Age = 14 },
-            new PassengerRequest { FareType = "student" },
-            new PassengerRequest { Age = 5, FareType = "contract_bulk_child" }
         },
                     MaxConnections = 0,
-                    CabinClass = "economy"
+                    
                 }
             };
             return offerRequest;
