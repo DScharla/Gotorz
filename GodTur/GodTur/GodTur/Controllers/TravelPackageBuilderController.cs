0b0724b33334a20eb5eb93fa6e4ed32a5fe59099
Christina Løvenholt Andersen <clan63283@edu.ucl.dk>
2025-05-02 12:38:27 +0200

    Første iteration på at gemme travelpackage i db

---
+++ GodTur/GodTur/GodTur/Controllers/TravelPackageBuilderController.cs
@@ -0,0 +1,71 @@
+﻿using GodTur.Models;
+using GodTur.Models.Context;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace GodTur.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TravelPackageBuilderController : ControllerBase
+    {
+        private readonly OfferResponseContext _context;
+        public TravelPackageBuilderController(OfferResponseContext context)
+        {
+            _context = context;
+        }
+        [HttpPost, Route("create")]
+
+        public async Task Create([FromBody] TravelPackageDTO travelPackageDTO)
+        {
+            TravelPackageDTO travelPackage = travelPackageDTO;
+            
+            City hotelCity = _context.Cities.Find(travelPackage.PackageHotel.City);
+
+            if (hotelCity != null)
+            {
+                _context.Hotels.Add(new Hotel
+                {
+                    Name = travelPackage.PackageHotel.HotelName,
+                    CityId = hotelCity.CityId,
+                    StayPrice = travelPackage.PackageHotel.Price,
+                    CheckInDate = travelPackage.PackageHotel.CheckInDate,
+                    CheckOutDate = travelPackage.PackageHotel.CheckOutDate,
+                });
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+
+                City newCity = new City
+                {
+                    Name = travelPackage.PackageHotel.City,
+                    IataCountryCode = travelPackage.PackageHotel.Country
+                };
+                _context.Cities.Add(newCity);
+
+                await _context.SaveChangesAsync();
+
+                _context.Hotels.Add(new Hotel
+                {
+                    Name = travelPackage.PackageHotel.HotelName,
+                    CityId = newCity.CityId,
+                    StayPrice = travelPackage.PackageHotel.Price,
+                    CheckInDate = travelPackage.PackageHotel.CheckInDate,
+                    CheckOutDate = travelPackage.PackageHotel.CheckOutDate,
+                });
+
+                await _context.SaveChangesAsync();
+            }
+            // vi er ved at se på CRUD-funktionaliteten ift. hoteller og flyvninger, pris på flyet giver nogle udfordringer
+            //_context.Flights.Add(new Flight
+            //{
+            //    DepartingAt = travelPackage.OutboundFlight.DepartureDate,
+            //    ArrivingAt = travelPackage.ReturnFlight.ReturnDate,
+            //}
+
+            //    _context.TravelPackages.Add(travelPackage);
+
+        }
+    }
+}
---
+++ GodTur/Shared/TravelPackageDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class TravelPackageDTO
+    {
+        public StayDTO PackageHotel { get; set; }
+        public FlightDTO OutboundFlight { get; set; }
+        public FlightDTO ReturnFlight { get; set; }
+
+    }
+}
