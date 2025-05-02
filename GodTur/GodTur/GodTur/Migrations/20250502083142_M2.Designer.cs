c116351bc7123fd55c7da8372708a0173a009a7f
Trollerolle <tjtr63311@edu.ucl.dk>
2025-05-02 10:34:57 +0200

    Updateret migration.

--- GodTur/GodTur/GodTur/Migrations/20250502083142_M2.Designer.cs
+++ GodTur/GodTur/GodTur/Migrations/20250502083142_M2.Designer.cs
@@ -4,6 +4,7 @@ using GodTur.Models.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GodTur.Migrations
 {
     [DbContext(typeof(OfferResponseContext))]
-    partial class OfferResponseContextModelSnapshot : ModelSnapshot
+    [Migration("20250502083142_M2")]
+    partial class M2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
