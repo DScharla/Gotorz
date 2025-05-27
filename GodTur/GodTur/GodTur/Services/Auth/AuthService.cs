f32c6d4cd49da1bda89867a5ce846cc83ad7d754
Scharla <dsan62737>
2025-05-27 09:55:30 +0200

    15 min

--- GodTur/GodTur/GodTur/Services/Auth/AuthService.cs
+++ GodTur/GodTur/GodTur/Services/Auth/AuthService.cs
@@ -90,7 +90,7 @@ namespace GodTur.Services.Auth
 			var token = new JwtSecurityToken(
 				issuer: _configuration["JWT:Issuer"],
 				audience: _configuration["JWT:Audience"],
-				expires: DateTime.UtcNow.AddMinutes(1),
+				expires: DateTime.UtcNow.AddMinutes(15),
 				claims: authClaims,
 				signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256));
 
